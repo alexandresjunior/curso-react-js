a10e2fb597acd1bc4fe42217cf0def89fb8b547f
alexandresjunior <souzajr.alexandre@gmail.com>
2023-03-13 21:46:59 -0300

    Uso do react router dom para navegação entre páginas

--- meu-primeiro-projeto/src/App.js
+++ meu-primeiro-projeto/src/App.js
@@ -1,14 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Cabecalho from "./componentes/Cabecalho";
 import Rodape from "./componentes/Rodape";
+import Consultoria from "./paginas/Consultoria";
 import Cursos from "./paginas/Cursos";
+import Home from "./paginas/Home";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter>
       <Cabecalho />
-      <Cursos />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cursos" element={<Cursos />} />
+        <Route path="/consultoria" element={<Consultoria />} />
+      </Routes>
       <Rodape />
-    </>
+    </BrowserRouter>
   );
 }
 
--- meu-primeiro-projeto/src/componentes/Cabecalho.js
+++ meu-primeiro-projeto/src/componentes/Cabecalho.js
@@ -1,21 +1,23 @@
+import { Link } from "react-router-dom";
+
 const Cabecalho = () => {
     return (
         <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
             <div className="container-fluid px-5">
-                <a className="navbar-brand" href="./">Treina Recife</a>
+                <Link className="navbar-brand" to="/">Treina Recife</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="./">Home</a>
+                            <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="./">Cursos</a>
+                            <Link className="nav-link" to="/cursos">Cursos</Link>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="./">Consultoria</a>
+                            <Link className="nav-link" to="/consultoria">Consultoria</Link>
                         </li>
                         <form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Digite sua busca..." aria-label="Search" />
