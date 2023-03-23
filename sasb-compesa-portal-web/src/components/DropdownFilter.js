a0894d5881a2681de80f4e6f57cc68e0d4f56861
alexandresjunior <souzajr.alexandre@gmail.com>
2023-03-22 21:46:23 -0300

    Melhoria nos estilos dos componentes

--- sasb-compesa-portal-web/src/components/DropdownFilter.js
+++ sasb-compesa-portal-web/src/components/DropdownFilter.js
@@ -1,7 +1,7 @@
 const DropdownFilter = ({ options }) => {
   return (
     <div className="input-group mb-3 sasb-shadow">
-      <select className="form-select">
+      <select className="form-select shadow-none">
         {options?.map((option, index) => {
           return <option value={index} key={index}>{option}</option>;
         })}
--- sasb-compesa-portal-web/src/components/SearchBar.js
+++ sasb-compesa-portal-web/src/components/SearchBar.js
@@ -1,12 +1,20 @@
+import { FaSearch } from 'react-icons/fa';
+
 const SearchBar = () => {
   return (
-    <div className="input-group rounded mb-3">
+    <div className="input-group mb-3 sasb-shadow">
       <input
         type="text"
-        className="form-control"
-        placeholder="Digite o nome da barragem"
-      />
+        className="form-control border border-secondary border-end-0 shadow-none"
+        placeholder="Digite o nome da barragem" />
+      <button
+        className="btn border border-secondary bg-white border-start-0"
+        type="button">
+        <FaSearch />
+      </button>
     </div>
+
+
   );
 };
 
